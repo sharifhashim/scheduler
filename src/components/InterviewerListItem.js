b39a16c7efd47922338992e987100421bc11c5d9
Hashim Sharif <sharif.hashim@gmail.com>
2021-12-08 14:01:54 -0500

    create component and style sheet

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classNames from "classnames";
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem(props) {
+  const selectedClass = classNames("interviewers__item", {
+    'interviewers__item--selected': props.selected
+  })
+  return (
+    <li className={selectedClass} onClick={() => props.setInterviewer(props.id)}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+    {props.selected && props.name}
+    </li>
+  );
+}
