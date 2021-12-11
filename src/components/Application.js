e3f01edac89a6a6b17b29a7b047ba59bbfc5783f
Hashim Sharif <sharif.hashim@gmail.com>
2021-12-10 19:23:46 -0500

    implement getInterview function

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,15 +3,16 @@ import axios from 'axios';
 import "components/Application.scss";
 import DayList from 'components/DayList';
 import Appointment from "components/Appointment"
-import getAppointmentsForDay from "../helpers/selectors"
+import {getAppointmentsForDay, getInterview} from "../helpers/selectors"
 
 export default function Application(props) {
   const [state, setState] = useState({ 
     day: "Monday", 
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
-  const dailyAppointments = getAppointmentsForDay( state, state.day);
+  const dailyAppointments = getAppointmentsForDay( state, state.day );
   const setDay = day => setState(prev => ({ ...prev, day }));
   // const setDays = (days) => {
   //   setState({...state, days})
@@ -22,10 +23,11 @@ export default function Application(props) {
   useEffect(() => {
     Promise.all([
       axios.get("api/days"),
-      axios.get("api/appointments")
+      axios.get("api/appointments"),
+      axios.get("api/interviewers")
     ]).then((all) => {
-      console.log(all[1].data)
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}))
+      console.log(all[2].data)
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
     })
   }, [])
   
@@ -53,7 +55,12 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {dailyAppointments.map((appoint) => <Appointment key={dailyAppointments.id} {...appoint}/>)}
+        {dailyAppointments.map((appoint) => {
+          const interview = getInterview(state, appoint.interview)
+          return (
+
+            <Appointment key={appoint.id} id={appoint.id} time={appoint.time} interview={interview} />)}
+          )}
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
       </section>
     </main>
