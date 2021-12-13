08976de8d1189d2ee17b7ebd08303f4800be830f
Hashim Sharif <sharif.hashim@gmail.com>
2021-12-13 15:35:34 -0500

    when promise resolves from bookInterview function transition to Show component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,15 +5,27 @@ import Show from "components/Appointment/Show"
 import Empty from "components/Appointment/Empty"
 import Form from "components/Appointment/Form"
 import useVisualMode from "../../hooks/useVisualMode.js" 
+import Status from './Status.js';
 
 export default function Appointment(props) {
   const EMPTY = 'EMPTY';
   const SHOW = 'SHOW';
-  const CREATE = 'CREATE'
+  const CREATE = 'CREATE';
+  const SAVING = 'SAVING';
   const {mode, transition, back} = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
-
+  const save = function(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    transition(SAVING)
+    props.bookInterview(props.id, interview)
+    .then(() => transition(SHOW))
+    .catch(err => console.log(err))
+    
+  }
   return ( 
     <article className="appointment">
       {props.time ? props.time : "No Appointments"}
@@ -24,8 +36,8 @@ export default function Appointment(props) {
           interviewer={props.interview.interviewer}
           />
       )}
-      {mode === CREATE && <Form interviewers={props.interviewers} onSave={transition} onCancel={back} />}
-   
+      {mode === CREATE && <Form interviewers={props.interviewers} onSave={save} onCancel={back} />}
+      {mode === SAVING && <Status />}
     </article>
   );
 }
