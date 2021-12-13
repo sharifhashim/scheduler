b138746b295a8d938a14de2245153a8a7a9b01cf
Hashim Sharif <sharif.hashim@gmail.com>
2021-12-12 19:42:26 -0500

    import custom hook in index.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -26,7 +26,7 @@ export default function Application(props) {
       axios.get("api/appointments"),
       axios.get("api/interviewers")
     ]).then((all) => {
-      console.log(all[2].data)
+      //console.log(all[2].data)
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
     })
   }, [])
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,13 +3,29 @@ import "components/Appointment/styles.scss"
 import Header from "components/Appointment/Header"
 import Show from "components/Appointment/Show"
 import Empty from "components/Appointment/Empty"
+import Form from "components/Appointment/Form"
+import useVisualMode from "../../hooks/useVisualMode.js" 
 
 export default function Appointment(props) {
-  
+  const EMPTY = 'EMPTY';
+  const SHOW = 'SHOW';
+  const CREATE = 'CREATE'
+  const {mode, transition, back} = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return ( 
     <article className="appointment">
-    {props.time ? props.time : "No Appointments"}
-    {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
+      {props.time ? props.time : "No Appointments"}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show 
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+          />
+      )}
+      {mode === CREATE && <Form interviewers={[]} onSave={transition} onCancel={back} />}
+   
     </article>
   );
 }
