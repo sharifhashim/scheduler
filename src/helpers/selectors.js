288168af39ac1fe2310e2c3adda980ff423468e2
Hashim Sharif <sharif.hashim@gmail.com>
2021-12-12 20:11:12 -0500

    add tests for getInterviewersForDay function and create getInterviewersForDay function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -5,14 +5,9 @@ export function getAppointmentsForDay(state, day) {
       for (const item of date.appointments) {
         newArr.push(state.appointments[item])
       }
-      //console.log(state.appointments[1])
     }
-    //newArr.push(state.appointments[date.appointments])
-    //day.name === day
-
   });
   return newArr
-  //console.log(filteredAppointments)
 }
 
 export function getInterview(state, interview) {
@@ -27,8 +22,17 @@ export function getInterview(state, interview) {
   return newObj
 }
 
-
-
+export function getInterviewersForDay(state, day) {
+  const newArr = []
+  const filteredAppointments = state.days.filter(date => {
+    if (date.name === day) {
+      for (const item of date.interviewers) {
+        newArr.push(state.interviewers[item])
+      }
+    }
+  });
+  return newArr
+}
 // function selectUserByName(state, name) {
 //   const filteredNames = state.users.filter(user => user.name === name);
 //   return filteredNames;
